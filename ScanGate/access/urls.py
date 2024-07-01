bb6803c7c46dc594ef4dfb1c7e2697f21635f1ad
Angel Ceron Ramos <86043771+angxdes@users.noreply.github.com>
2024-07-01 00:20:01 -0600

    Update urls.py

--- ScanGate/access/urls.py
+++ ScanGate/access/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.access, name = 'access'),
-    path('perfil/<str:codigo_barras>/', views.perfil, name = 'perfil')
+    path('perfil/<str:codigo_barras>/', views.perfil, name = 'perfil'),
+    path('login/', views.login_view, name='login')
 ]
