8b4c6d34357c6bcd31221804ea9cd83ca39dc6bf
Angel Ceron Ramos <86043771+angxdes@users.noreply.github.com>
2024-07-01 00:43:45 -0600

    Update settings.py

--- ScanGate/ScanGate/settings.py
+++ ScanGate/ScanGate/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+LOGIN_URL = '/login/'  # Asegúrate de que esta URL coincide con tu vista de login
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
